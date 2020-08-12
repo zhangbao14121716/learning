35ea03c0f4ca83796775539aeb080e573ea2261a
doubleZ <1909039360@qq.com>
2020-08-12 20:56:15 +0800

    first commit

---
+++ java/com/atuigu/learning/Test.java
@@ -0,0 +1,15 @@
+package com.atuigu.learning;
+
+/**
+ * Author: doubleZ
+ * Datetime:2020/8/12   20:55
+ * Description:
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("1111111111111111111111111");
+        System.out.println("1111111111111111111111111");
+        System.out.println("1111111111111111111111111");
+        System.out.println("1111111111111111111111111");
+    }
+}
